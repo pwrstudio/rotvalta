33834c97517c19a9afa2234fc1397628d5957269
PWR <pwrpntr@gmail.com>
2024-09-16 21:06:11 +0200

    add label fields

--- packages/sanity/schemaTypes/ConversationList.ts
+++ packages/sanity/schemaTypes/ConversationList.ts
@@ -3,12 +3,36 @@ export default {
     title: 'Conversation list',
     name: 'conversationList',
     type: 'document',
+    groups: [
+        {
+            name: 'en',
+            title: 'English',
+        },
+        {
+            name: 'se',
+            title: 'Swedish',
+        },
+    ],
     fields: [
         {
             title: 'Title',
             name: 'title',
             type: 'string',
             readOnly: true,
+            validation: (Rule: any) => Rule.required()
+        },
+        {
+            title: "Label (Swedish)",
+            name: "label_se",
+            type: "string",
+            group: 'se',
+            validation: (Rule: any) => Rule.required()
+        },
+        {
+            title: "Label (English)",
+            name: "label_en",
+            type: "string",
+            group: 'en',
             validation: (Rule: any) => Rule.required()
         },
         {
